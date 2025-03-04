34a1649d36700968cec69ab9d1301b20a2597cf7
Mayowa Esan <mayowa.esan@garmin.com>
2025-03-04 13:52:23 -0700

    fix: update server link

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -28,7 +28,7 @@ const appname = import.meta.env.VITE_APP_NAME;
         class="flex w-full z-10 items-center justify-between gap-5 p-3 duration-500 transition-colors border-b-1 border-transparent">
         <a href="/" class="text-color ml-2 font-semibold">{{ appname }}</a>
         <div class="flex gap-2">
-            <Button as="a" href="https://github.com/Battlesquid/qnaplus" target="_blank" class="header-btn"
+            <Button as="a" href="https://nexus.qnapl.us" target="_blank" class="header-btn"
             icon="pi pi-discord" aria-label="Discord Server" text />
             <Button as="a" href="https://github.com/qnaplus" target="_blank" class="header-btn"
                 icon="pi pi-github" aria-label="Github" text />
